75cb469e89eef8a9599f738d501d8bfffa2199a0
Michael Hunger <michael@crewmeister.com>
2020-12-16 11:54:48 +0100

    Crewmeister Java Coding Challenge
    - Added Basic Project from initializr.spring.io
    - Created demo API

---
+++ src/main/java/com/crewmeister/cmcodingchallenge/currency/CurrencyConversionRates.java
@@ -0,0 +1,17 @@
+package com.crewmeister.cmcodingchallenge.currency;
+
+public class CurrencyConversionRates {
+    private double conversionRate;
+
+    public CurrencyConversionRates(double conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+
+    public double getConversionRate() {
+        return conversionRate;
+    }
+
+    public void setConversionRate(double conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+}
